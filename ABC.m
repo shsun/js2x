24d1280fd7169cd49b647ed6db3613faf4d953e7
sunshanghai <sunshanghai@baidu.com>
2016-04-03 14:12:21 +0800

    init

---
+++ ABC.m
@@ -0,0 +1,5 @@
+/* The file is supposed to be automatically generated from mraid.js to escaped string for NSString constant literal. */
+NSString * const XAD_MRAID_INJECTION_JS
+=@"(function(){window.mraid=new function(){this.listeners={};this.viewable=!0;this.addEventListener=function(b,d){\"undefined\"===typeof this.listeners[b]&&(this.listeners[b]=[]);this.listeners[b].push(d)};this.removeEventListener=function(b,d){if(this.listeners[b]instanceof Array){var a=this.listeners[b],c,e;if(null!==a){e=a.length;for(c=0;c<e;c++)if(a[c]===d){a.splice(c,1);break}}}};this.dispatchEvent=function(b){if(this.listeners[b]instanceof Array){var d=this.listeners[b],a,c;c=d.length;for(a=0;a<\n\
+c;a++)try{d[a]&&d[a].apply(this,Array.prototype.slice.call(arguments,1))}catch(e){this._xad_log(\"Exception in dispatchEvent:\"+e)}}};this.getVersion=function(){return\"2.0\"}}})();\n\
+";
